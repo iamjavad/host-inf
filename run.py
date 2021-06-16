9afabe467002e24232e783dedc2270e29862d212
javad pournosrat <pournosrat.javad@gmail.com>
2021-06-16 11:20:52 +0430

    complet location information

--- run.py
+++ run.py
@@ -4,18 +4,24 @@ from kavenegar import APIException
 import shodan
 import os
 
-KEY = "NDciAIz4884Y2EaFjDVzBmHMXer4sTbC"
+KEY = ""
 
 api = shodan.Shodan(KEY)
 
-host = api.host("185.190.39.234")
+host = api.host("")
 
-print(host['ip_str'])
+print("ip address: {}".format(host['ip_str']))
 
-print(host['org'])
+print("org: {}".format(host['org']))
 
-print(host['os'])
+print("os: {}".format(host['os']))
 
 for item in host['data']:
     print(item['port'])
-    print(item['location']['city'])
+    print("country: {}".format(item['location']['country_name']))
+    print("country code: {}".format(item['location']['country_code']))
+    print("city: {}".format(item['location']['city']))
+    print("region code: {}".format(item['location']['region_code']))
+    print("latitude: {}".format(item['location']['latitude']))
+    print("longitude: {}".format(item['location']['longitude']))
+    print(item['location'])
